6de5288c01583b50cc4677c3c7c0c409f327e3bb
Harvey Taylor <harveytaylor457@gmail.com>
2025-12-06 23:14:25 +0000

    added OS detection and some os specific commands for future logs

--- include/Platform.h
+++ include/Platform.h
@@ -1,14 +1,21 @@
 #pragma once
 #include <string>
+#include <cstdint>
 
-namespace OSUtilities {
+namespace harvey::Platform {
+
     bool isWindows();
     bool isLinux();
     bool isMac();
 
-    int getProcessID();
-    int getThreadID();
+    std::uint32_t getProcessID();
+    std::uint64_t getThreadID();
 
-    std::string getExecutableName();
     std::string getExecutablePath();
+    std::string getExecutableName();
+
+    bool createDirectory(const std::string& path);
+    bool fileExists(const std::string& path);
+
+    bool setFilePermissionsOwnerOnly(const std::string& path);
 }
--- src/Platform.cpp
+++ src/Platform.cpp
@@ -1,33 +1,88 @@
 #include "../include/Platform.h"
 
+#if defined(_WIN32) || defined(_WIN64)
+    #include <windows.h>
+    #include <processthreadsapi.h>
+#elif defined(__linux__)
+    #include <unistd.h>
+    #include <sys/types.h>
+    #include <sys/stat.h>
+    #include <pthread.h>
+#elif defined(__APPLE__)
+    #include <unistd.h>
+    #include <sys/types.h>
+    #include <sys/stat.h>
+    #include <pthread.h>
+#endif
 
 namespace platform {
-    
+
+    // -----------------------------------
+    // OS Detection
+    // -----------------------------------
     bool isWindows() {
-        //Check for windows
+        #ifdef _WIN32
+            return true;
+        #endif
     }
 
     bool isLinux() {
-        //check for linux
+        #ifdef __linux__
+            return true;
+        #endif
     }
 
     bool isMac() {
-        //check for mac
+        #ifdef __APPLE__
+            return true;
+        #endif
     }
 
-    int getProcessID() {
-        //get PID
+    // get PID
+    std::uint32_t getProcessID() {
+        if (isWindows()) 
+        {
+            return std::uint32_t(GetCurrentProcessId());
+        } else if (isLinux() || isMac()) 
+        {
+            return std::uint32_t(getpid());
+        }
     }
 
-    int getThreadID() {
-        //get thread number
+    // get TID
+    std::uint64_t getThreadID() {
+        if (isWindows())
+        {
+            return std::uint64_t(GetCurrentThreadId());
+        } else if (isLinux() || isMac())
+        {
+            return std::uint64_t(pthread_self());
+        }
     }
 
     std::string getExecutableName() {
-        //get name of executable running
+        if (isWindows())
+        {
+            char store[MAX_PATH];
+            GetModuleFileNameA(NULL, store, MAX_PATH);
+
+            std::string fullPath(store);
+            auto pos = fullPath.find_last_of("\\/");
+
+        }
+        
     }
 
-    std::string getExecutablePath() {
-        //get path for the executable
+    bool createDirectory(const std::string& path) {
+        // mkdir() compatible with both?
     }
+
+    bool fileExists(const std::string& path) {
+        // stat() and GetFileAttributes()
+    }
+
+    bool setFilePermissionsOwnerOnly(const std::string& path) {
+        // chmod() more research for windows
+    }
+
 }
